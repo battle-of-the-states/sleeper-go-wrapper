03a0be7e9bb578a29e47fd72ce16215e1e7ed8bf
abspen1 <abspencer2097@gmail.com>
2022-08-02 11:15:09 -0700

    add leagues

---
+++ leagues.go
@@ -0,0 +1,236 @@
+package sleeper
+
+import (
+	"fmt"
+)
+
+// LeaguesInfoJSON is the return type for all leagues for user
+type LeaguesInfoJSON []LeagueInfoJSON
+
+// LeagueInfoJSON is the return type of the league info API.
+type LeagueInfoJSON struct {
+	TotalRosters int    `json:"total_rosters"`
+	Status       string `json:"status"`
+	Sport        string `json:"sport"`
+	Shard        int    `json:"shard"`
+	Settings     struct {
+		MaxKeepers           int `json:"max_keepers"`
+		DraftRounds          int `json:"draft_rounds"`
+		TradeReviewDays      int `json:"trade_review_days"`
+		Squads               int `json:"squads"`
+		ReserveAllowDnr      int `json:"reserve_allow_dnr"`
+		CapacityOverride     int `json:"capacity_override"`
+		PickTrading          int `json:"pick_trading"`
+		DisableTrades        int `json:"disable_trades"`
+		TaxiYears            int `json:"taxi_years"`
+		TaxiAllowVets        int `json:"taxi_allow_vets"`
+		BestBall             int `json:"best_ball"`
+		LastReport           int `json:"last_report"`
+		DisableAdds          int `json:"disable_adds"`
+		WaiverType           int `json:"waiver_type"`
+		BenchLock            int `json:"bench_lock"`
+		ReserveAllowSus      int `json:"reserve_allow_sus"`
+		Type                 int `json:"type"`
+		ReserveAllowCov      int `json:"reserve_allow_cov"`
+		WaiverClearDays      int `json:"waiver_clear_days"`
+		DailyWaiversLastRan  int `json:"daily_waivers_last_ran"`
+		WaiverDayOfWeek      int `json:"waiver_day_of_week"`
+		StartWeek            int `json:"start_week"`
+		PlayoffTeams         int `json:"playoff_teams"`
+		NumTeams             int `json:"num_teams"`
+		ReserveSlots         int `json:"reserve_slots"`
+		PlayoffRoundType     int `json:"playoff_round_type"`
+		DailyWaiversHour     int `json:"daily_waivers_hour"`
+		WaiverBudget         int `json:"waiver_budget"`
+		ReserveAllowOut      int `json:"reserve_allow_out"`
+		OffseasonAdds        int `json:"offseason_adds"`
+		PlayoffSeedType      int `json:"playoff_seed_type"`
+		DailyWaivers         int `json:"daily_waivers"`
+		PlayoffWeekStart     int `json:"playoff_week_start"`
+		DailyWaiversDays     int `json:"daily_waivers_days"`
+		LeagueAverageMatch   int `json:"league_average_match"`
+		Leg                  int `json:"leg"`
+		TradeDeadline        int `json:"trade_deadline"`
+		ReserveAllowDoubtful int `json:"reserve_allow_doubtful"`
+		TaxiDeadline         int `json:"taxi_deadline"`
+		ReserveAllowNa       int `json:"reserve_allow_na"`
+		TaxiSlots            int `json:"taxi_slots"`
+		PlayoffType          int `json:"playoff_type"`
+	} `json:"settings"`
+	SeasonType       string             `json:"season_type"`
+	Season           string             `json:"season"`
+	ScoringSettings  map[string]float64 `json:"scoring_settings"`
+	RosterPositions  []string           `json:"roster_positions"`
+	PreviousLeagueID string             `json:"previous_league_id"`
+	Name             string             `json:"name"`
+	Metadata         struct {
+		LatestLeagueWinnerRosterID string `json:"latest_league_winner_roster_id"`
+		Continued                  string `json:"continued"`
+	} `json:"metadata"`
+	LoserBracketID        interface{} `json:"loser_bracket_id"`
+	LeagueID              string      `json:"league_id"`
+	LastReadID            string      `json:"last_read_id"`
+	LastPinnedMessageID   string      `json:"last_pinned_message_id"`
+	LastMessageTime       int64       `json:"last_message_time"`
+	LastMessageTextMap    interface{} `json:"last_message_text_map"`
+	LastMessageID         string      `json:"last_message_id"`
+	LastMessageAttachment interface{} `json:"last_message_attachment"`
+	LastAuthorIsBot       bool        `json:"last_author_is_bot"`
+	LastAuthorID          string      `json:"last_author_id"`
+	LastAuthorDisplayName string      `json:"last_author_display_name"`
+	LastAuthorAvatar      string      `json:"last_author_avatar"`
+	GroupID               interface{} `json:"group_id"`
+	DraftID               string      `json:"draft_id"`
+	CompanyID             interface{} `json:"company_id"`
+	BracketID             int64       `json:"bracket_id"`
+	Avatar                string      `json:"avatar"`
+}
+
+// RostersJSON is the return type for all rosters in a league
+type RostersJSON []RosterJSON
+
+// RosterJSON is a single roster from the league rosters API.
+type RosterJSON struct {
+	Taxi     []string `json:"taxi"`
+	Starters []string `json:"starters"`
+	Settings struct {
+		Wins             int `json:"wins"`
+		WaiverPosition   int `json:"waiver_position"`
+		WaiverBudgetUsed int `json:"waiver_budget_used"`
+		TotalMoves       int `json:"total_moves"`
+		Ties             int `json:"ties"`
+		Losses           int `json:"losses"`
+		Fpts             int `json:"fpts"`
+	} `json:"settings"`
+	RosterID  int         `json:"roster_id"`
+	Reserve   []string    `json:"reserve"`
+	Players   []string    `json:"players"`
+	PlayerMap interface{} `json:"player_map"`
+	OwnerID   string      `json:"owner_id"`
+	LeagueID  string      `json:"league_id"`
+	CoOwners  interface{} `json:"co_owners"`
+	// known keys:
+	// streak, record, p_nick_<player ID>, allow_pn_scoring, allow_pn_news
+	Metadata interface{} `json:"metadata"`
+}
+
+// MatchupsJSON is the return type for all matchups for the week
+type MatchupsJSON []MatchupJSON
+
+// MatchupJSON is a single matchup from the league matchups API.
+type MatchupJSON struct {
+	StartersPoints []float32          `json:"starters_points"`
+	Starters       []string           `json:"starters"`
+	RosterID       int                `json:"roster_id"`
+	Points         float32            `json:"points"`
+	PlayerPoints   map[string]float32 `json:"player_points"`
+	Players        []string           `json:"players"`
+	MatchupID      int                `json:"matchup_id"`
+	CustomPoints   float32            `json:"custom_points"`
+}
+
+/*
+This endpoint retrieves all leagues for a user.
+
+sport	(required) : We only support "nfl" right now.
+userID (required) : The numerical ID of the user.
+season	(required) : Season can be 2017, 2018, etc...
+*/
+func (c *Client) GetLeaguesForUser(sport Sport, userID, season string) (LeaguesInfoJSON, error) {
+	// https://api.sleeper.app/v1/user/<user_id>/leagues/<sport>/<season>
+	lastfmURL := fmt.Sprintf("%s/user/%s/leagues/%s/%s", c.sleeperURL, userID, sport, season)
+
+	leagues := new(LeaguesInfoJSON)
+
+	err := c.get(lastfmURL, leagues)
+
+	if err != nil {
+		return *leagues, err
+	}
+
+	return *leagues, nil
+}
+
+/*
+This endpoint retrieves all leagues for a user.
+
+leagueID (required) : The ID of the league to retrieve
+*/
+func (c *Client) GetLeague(leagueID string) (LeagueInfoJSON, error) {
+	// https://api.sleeper.app/v1/league/<league_id>
+	lastfmURL := fmt.Sprintf("%s/league/%s", c.sleeperURL, leagueID)
+
+	league := new(LeagueInfoJSON)
+
+	err := c.get(lastfmURL, league)
+
+	if err != nil {
+		return *league, err
+	}
+
+	return *league, nil
+}
+
+/*
+This endpoint retrieves all rosters in a league.
+
+leagueID (required) : The ID of the league to retrieve
+*/
+func (c *Client) GetLeagueRosters(leagueID string) (RostersJSON, error) {
+	// https://api.sleeper.app/v1/league/<league_id>/rosters
+	lastfmURL := fmt.Sprintf("%s/league/%s/rosters", c.sleeperURL, leagueID)
+
+	rosters := new(RostersJSON)
+
+	err := c.get(lastfmURL, rosters)
+
+	if err != nil {
+		return *rosters, err
+	}
+
+	return *rosters, nil
+}
+
+/*
+This endpoint retrieves all users in a league.
+
+This also includes each user's display_name, avatar, and their
+metadata which sometimes includes a nickname they gave their team.
+
+leagueID (required) : The ID of the league to retrieve
+*/
+func (c *Client) GetLeagueUsers(leagueID string) (UsersJSON, error) {
+	// https://api.sleeper.app/v1/league/<league_id>/users
+	lastfmURL := fmt.Sprintf("%s/league/%s/users", c.sleeperURL, leagueID)
+
+	users := new(UsersJSON)
+
+	err := c.get(lastfmURL, users)
+
+	if err != nil {
+		return *users, err
+	}
+
+	return *users, nil
+}
+
+/*
+This endpoint retrieves all users in a league.
+
+leagueID (required) : The ID of the league to retrieve
+week 	 (required) : The week number to get the matchups
+*/
+func (c *Client) GetLeagueMatchups(leagueID string, week int8) (MatchupsJSON, error) {
+	// https://api.sleeper.app/v1/league/<league_id>/matchups/<week>
+	lastfmURL := fmt.Sprintf("%s/league/%s/matchups/%d", c.sleeperURL, leagueID, week)
+
+	matchups := new(MatchupsJSON)
+
+	err := c.get(lastfmURL, matchups)
+
+	if err != nil {
+		return *matchups, err
+	}
+
+	return *matchups, nil
+}
---
+++ leagues_test.go
@@ -0,0 +1,148 @@
+package sleeper
+
+import (
+	"testing"
+)
+
+func TestGetLeaguesForUser(t *testing.T) {
+	client, err = NewClient(&hClient)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.GetLeaguesForUser(NFL, "446778421305929728", "2020")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var tests = []struct {
+		test     int
+		expected int
+	}{
+		{len(res), 6},
+	}
+
+	for _, test := range tests {
+		if test.test != test.expected {
+			t.Errorf("Test Failed: %d expected, received: %d", test.expected, test.test)
+		}
+	}
+}
+
+func TestGetLeague(t *testing.T) {
+	client, err = NewClient(&hClient)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.GetLeague("732292628472778752")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var tests = []struct {
+		test     string
+		expected string
+	}{
+		{res.Status, "complete"},
+		{res.Sport, "nfl"},
+		{res.SeasonType, "regular"},
+		{res.Season, "2021"},
+		{res.Name, "Sages Fantasy Football"},
+	}
+
+	for _, test := range tests {
+		if test.test != test.expected {
+			t.Errorf("Test Failed: %s expected, received: %s", test.expected, test.test)
+		}
+	}
+}
+
+func TestGetLeagueRosters(t *testing.T) {
+	client, err = NewClient(&hClient)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.GetLeagueRosters("732292628472778752")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var tests = []struct {
+		test     string
+		expected string
+	}{
+		{res[0].OwnerID, "332664810955108352"},
+		{res[0].LeagueID, "732292628472778752"},
+	}
+
+	for _, test := range tests {
+		if test.test != test.expected {
+			t.Errorf("Test Failed: %s expected, received: %s", test.expected, test.test)
+		}
+	}
+}
+
+func TestGetLeagueUsers(t *testing.T) {
+	client, err = NewClient(&hClient)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.GetLeagueUsers("732292628472778752")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var tests = []struct {
+		test     string
+		expected string
+	}{
+		{res[0].UserID, "332664810955108352"},
+		{res[0].LeagueID, "732292628472778752"},
+		{res[0].Metadata.MentionPn, "on"},
+	}
+
+	for _, test := range tests {
+		if test.test != test.expected {
+			t.Errorf("Test Failed: %s expected, received: %s", test.expected, test.test)
+		}
+	}
+}
+
+func TestGetLeagueMatchups(t *testing.T) {
+	client, err = NewClient(&hClient)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	res, err := client.GetLeagueMatchups("732292628472778752", 4)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	var tests = []struct {
+		test     int
+		expected int
+	}{
+		{res[0].RosterID, 1},
+		{res[0].MatchupID, 4},
+	}
+
+	for _, test := range tests {
+		if test.test != test.expected {
+			t.Errorf("Test Failed: %d expected, received: %d", test.expected, test.test)
+		}
+	}
+}
